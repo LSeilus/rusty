aa727844c96357b18f8b75ac692c278dc286c2b3
LSeilus <lseilus@tutanota.com>
2022-01-07 19:08:28 +0100

    sleep in main

--- main.cpp
+++ main.cpp
@@ -1,9 +1,17 @@
 #include <iostream>
+/*Apparently, a sleep() function is not provided standard,
+and requires different libraries depending on the OS, hence the if statement.*/
+#ifdef _WIN32
+#include <Windows.h>
+#else
+#include <unistd.h>
+#endif
 
 int main()
 {
-    std::cout << "no no no";
-    //commeeeent 1
-    //comment 2
+    std::cout << "Devon is cute!";
+    sleep(100);
+    std::cout << "Super cute, even!";
+
     return;
 }
