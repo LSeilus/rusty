cbe9a4c4e98831fd9493cae90aaf02aa1c930abf
LSeilus <lseilus@tutanota.com>
2022-01-08 22:48:24 +0100

    olcPixelGameEngine

--- main.cpp
+++ main.cpp
@@ -6,12 +6,33 @@ and requires different libraries depending on the OS, hence the if statement.*/
 #else
 #include <unistd.h>
 #endif
+#define OLC_PGE_APPLICATION
+#include "./PGE/olcPixelGameEngine.h"
+#include <assert.h>
 
-int main()
-{
-    std::cout << "Devon is cute!";
-    sleep(100);
-    std::cout << "Super cute, even!";
+class Window : public olc::PixelGameEngine{
+    public:
+    Window(){
+        sAppName = "Just screwing around";
+    }
 
-    return;
+    bool OnUserCreate() override{
+        return true;
+    }
+
+    bool OnUserUpdate(float fElapsedTime) override{
+        DrawLine(10, 10, ScreenWidth() - 10, 10, olc::RED);
+		DrawLine(10, 10, 10, ScreenHeight() - 10, olc::RED);
+		DrawLine(ScreenWidth() - 10, 10, ScreenWidth() - 10, ScreenHeight() - 10, olc::RED);
+        DrawLine(ScreenWidth() - 10, ScreenHeight() - 10, ScreenWidth() - 10, ScreenHeight() - 10, olc::RED);
+        return true;
+    }
+
+};
+
+int main(){
+    Window win;
+    if (win.Construct(512,256, 2, 2))
+        win.Start();
+    return 0;
 }
